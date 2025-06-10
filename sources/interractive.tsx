53c1251e05e7a05d186f33a2c076cf417f215e9b
Thomas Trépanier <thomtrepanier@gmail.com>
2025-06-10 09:41:30 -0400

    Make workspace optional

--- sources/interractive.tsx
+++ sources/interractive.tsx
@@ -73,7 +73,7 @@ export default class UpgradeInteractiveCommand extends BaseCommand {
     ],
   });
 
-  workspaces = Option.Rest({ required: 1 });
+  workspaces = Option.Rest();
 
   excludedDeps = Option.String(`--exclude`, {
     description: `A comma-separated list of dependencies to exclude from the upgrade (supports glob patterns like "@types/*" or "react-*")`,
